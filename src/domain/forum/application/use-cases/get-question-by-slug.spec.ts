38148f543a03c62ab92c5b65838b55e1b5c0b34d
Erick <erickcestari03@gmail.com>
2023-08-21 11:00:07 -0300

    Adding test get-question-by-slug

--- src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -3,17 +3,29 @@ import { GetQuestionBySlugUseCase } from './get-question-by-slug'
 import { Question } from '../../enterprise/entities/question'
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { Slug } from '../../enterprise/entities/value-objects/slug'
+import { CreateQuestionUseCase } from './create-question'
+import { makeQuestion } from 'test/factories/make-question'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let sut: GetQuestionBySlugUseCase
+let createQuestionUseCase: CreateQuestionUseCase
 
 describe('Get question by slug', () => {
   beforeEach(() => {
     inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
     sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository)
+    createQuestionUseCase = new CreateQuestionUseCase(inMemoryQuestionsRepository)
   })
 
   it('should be able to get a question', async () => {
+    /*
+    const {question: newQuestion}  = await createQuestionUseCase.execute({
+      authorId: new UniqueEntityId().toString(),
+      content: 'This is the question content',
+      title: ' Question Slug ',
+    })
+    */
+    /*
     const newQuestion = Question.create({
       authorId: new UniqueEntityId(),
       content: 'This is the question content',
@@ -21,6 +33,15 @@ describe('Get question by slug', () => {
     })
 
     inMemoryQuestionsRepository.create(newQuestion)
+    */
+
+    const newQuestion = makeQuestion({
+      slug: Slug.create('question-slug'),
+    })
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    console.log(newQuestion)
 
     const { question } = await sut.execute({
       slug: newQuestion.slug,
